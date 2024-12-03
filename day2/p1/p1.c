8d1657f3c40e3ebd4aebaaf87ff66ecf6a389dcb
ParrotsByte <parrotsbyte@protonmail.com>
2024-12-03 08:18:33 +0100

    day2 solutions

---
+++ day2/p1/p1.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_SIZE 8
+#define NUM_LINES 1000
+
+bool check_safety(int *array, int size) {
+    bool decreasing = false, increasing = false, anomaly = false;
+    int diff, a_diff;
+
+    for (int i = 1; i < size; i++) {
+        diff = array[i] - array[i - 1];
+        a_diff = abs(diff);
+
+        if (diff < 0)
+            increasing = true;
+        else if (diff > 0)
+            decreasing = true;
+        else {
+            anomaly = true;
+            break;
+        }
+            
+        if (increasing && decreasing) {
+            anomaly = true;
+            break;
+        }
+            
+        if (a_diff < 1 || a_diff > 3) {
+            anomaly = true;
+            break;
+        }
+    }
+
+    return !anomaly;
+}
+
+int main(int argc, char **argv)
+{
+    FILE *fp;
+    int ch, i = 0, num_safe = 0;
+    int num_spaces[NUM_LINES] = {0}, array[MAX_SIZE];
+
+    bool decreasing = false, increasing = false;
+    bool anomaly = false;
+
+    if ((fp = fopen(argv[1], "r")) == NULL) {
+        fprintf(stderr, "Couldn't open file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((ch = getc(fp)) != EOF) {
+        if (ch == '\n')
+            i++;
+        if (ch == ' ')
+            num_spaces[i]++;
+    }
+    rewind(fp);
+
+    for (int p = 0; p < NUM_LINES; p++) {
+        for (int q = 0; q < num_spaces[p] + 1; q++) {
+            fscanf(fp, "%d", array + q);
+        }
+
+        if (check_safety(array, num_spaces[p] + 1)) {
+            num_safe++;
+            continue;
+        }
+    }
+
+    printf("Number of safe entries: %d\n", num_safe);
+
+    fclose(fp);
+
+    return EXIT_SUCCESS;
+}
